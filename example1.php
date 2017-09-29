0d707ebade4bae35a6927762c742ab93184c4ca7
phuongngtpdn <phuongntp.dn@gmail.com>
2017-09-29 22:15:43 +0700

    add file

---
+++ example1.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+
+	<?php
+	echo "Hello World";
+	?>
+<br>
+	<?php
+	$numberone = 3;
+	$numbertwo = 5;
+	echo $numberone + $numbertwo;
+	echo "<br>";
+		if ($numberone > $numbertwo) {
+			echo "true";
+		} else if ($numberone <= $numbertwo) {
+			echo "false";
+		}
+	?>
+
+</body>
+</html>
